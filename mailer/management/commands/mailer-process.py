2ff2f99dedfc3eb0a563f453efdd20943161df0e
Christopher Conover <clcono@gmail.com>
2011-12-20 10:46:32 -0500

    Completed the processing script

--- mailer/management/commands/mailer-process.py
+++ mailer/management/commands/mailer-process.py
@@ -1,11 +1,12 @@
 from django.core.management.base import BaseCommand
-from mailer.models               import Email, Instance, RecipientGroup
+from mailer.models               import Email, Instance, RecipientGroup, Recipient, InstanceRecipientDetails
 from datetime                    import datetime, timedelta
 from django.conf                 import settings
 import calendar
 import hashlib
 import smtplib
 import logging
+import re
 
 log = logging.getLogger(__name__)
 
@@ -32,42 +33,86 @@ class Command(BaseCommand):
 			if email.recurrence == Email.Recurs.never and start == now_d:
 				todays_email_ids.append(email.id)
 			elif email.recurrence == Email.Recurs.daily:
-				todays_email_ids.append(email_id)
+				todays_email_ids.append(email.id)
 			elif email.recurrence == Email.Recurs.weekly and ((now_d - start).days % 7) == 0:
-				todays_email_ids.append(email_id)
+				todays_email_ids.append(email.id)
 			elif email.recurrence == Email.Recurs.monthly and now_d.day == start.day:
-				todays_email_ids.append(email_id)
+				todays_email_ids.append(email.id)
 			elif email.recurrence == Email.Recurs.yearly and ((now_d - start).days % 365) == 0:
-				today_emails_ids.append(email_id)
+				today_emails_ids.append(email.id)
+
+		log.debug('Emails occurring sometime today: ' + str(todays_email_ids))
 		
 		# Fetch all the emails that are due to be sent in the next 15 minutes
 		email_kwargs = {
 			'id__in'          :todays_email_ids,
-			'start_date__gte' :now_d,
 			'send_time__gte'  :now_t,
 			'send_time__lt'   :later_t}
 		emails = Email.objects.filter(**email_kwargs)
 
+		log.debug('Emails being sent in this run: ' + str(todays_email_ids))
+
 		for email in emails:
-			content      = email.content
+			content      = email.content.decode('ascii', errors='ignore')
 			subject      = email.title
-			# Which groups have already been sent to.
-			# Used to avoid sending an email to the same person
-			# more than once
-			sent_groups  = [] 
+
+			# These headers will be the same for every sent email
+			smtp_headers = {
+				'From: '         : email.smtp_from_address,
+				'Subject: '      : subject,
+				'Content-type: ' : 'text/html; charset=us-ascii',
+			}
 
 			instance = Instance(email=email, sent_html=content, in_progress=True)
 			instance.save()
 
 			for group in email.recipient_groups.all():
 				for recipient in group.recipients.all():
-					# This user isn't in any previous groups
-					if Recipient.objects.filter(groups__in=sent_groups, id=recipient.id).count() == 0:
-						from_address = 'webcom@ucf.edu'
-						to_address  = recipient.email_address
-						msg = 'From: $s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s' % ('webcom@ucf.edu', recipient.email_address, subject, content)
-						#print amazon_ses.send_mail(from_address, to_address, msg)
-				send_groups.append(group)
+
+					try:
+						# Check to see if this email has already been
+						# sent to this recipient
+						InstanceRecipientDetails.objects.get(instance=instance, recipient=recipient)
+					except InstanceRecipientDetails.DoesNotExist:
+
+						# Customize the content for this recipient
+						customized_content = content
+						for mapping in email.mappings.all():
+							find    = email.replace_delimiter + mapping.email_label + email.replace_delimiter
+							try:
+								replace = getattr(recipient, mapping.recipient_field) 
+							except AttributeError:
+								log.error('Invalid email label mapping from `%s` to `%s` on the recipient object' % (find, mapping.recipient_field))
+							else:
+								customized_content = customized_content.replace(find, replace)
+						
+						# Construct the final SMTP headers
+						final_smtp_headers = smtp_headers.copy()
+						final_smtp_headers['To: '] = recipient.smtp_address
+
+						msg = '\r\n'.join(list(k + v for k,v in final_smtp_headers.items())) + '\r\n\r\n' + customized_content
+
+						instance_details_kwargs = {
+							'instance'  :instance,
+							'recipient' :recipient,
+						}
+
+						try:
+							amazon_ses.sendmail(email.from_email_address, recipient.email_address, msg)
+						except smtplib.SMTPRecipientsRefused, e: # Exception Type 0
+							instance_details_kwargs['exception_type'] = 0
+							instance_details_kwargs['exception_msg']  = str(e)
+						except smtplib.SMTPHeloError, e:         # Exception type 1
+							instance_details_kwargs['exception_type'] = 1
+							instance_details_kwargs['exception_msg']  = str(e)
+						except smtplib.SMTPSenderRefused, e:     # Exception type 2
+							instance_details_kwargs['exception_type'] = 2
+							instance_details_kwargs['exception_msg']  = str(e)
+						except smtplib.SMTPDataError, e:         # Exception type 3
+							instance_details_kwargs['exception_type'] = 3
+							instance_details_kwargs['exception_msg']  = str(e)
+						instance_details = InstanceRecipientDetails(**instance_details_kwargs)
+						instance_details.save()
 			instance.in_progress = False
 			instance.end = datetime.now()
 			instance.save()
