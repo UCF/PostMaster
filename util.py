b737dbeb1d55d0cc487a477d9eaec342edca4d6f
Christopher Conover <clcono@gmail.com>
2011-12-20 11:57:57 -0500

    Removed the rest of the util code

--- util.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-try:
-	import requests
-except ImportError:
-	print 'This script utilizes the `requests` package.'
-	print 'Download and install it from http://pypi.python.org/pypi/requests'
