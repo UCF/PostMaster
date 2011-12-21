dc96df0e4292574606e41914fa1caf9eb307ff10
Christopher Conover <clcono@gmail.com>
2011-12-21 14:22:51 -0500

    Replaced email_id with instance_id

--- util.py
+++ util.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 import hmac
 
-def calc_url_mac(url, position, recipient, email_id):
-	mash = ''.join([str(url), str(position), str(recipient), str(email_id))
+def calc_url_mac(url, position, recipient, instance_id):
+	mash = ''.join([str(url), str(position), str(recipient), str(instance_id))
 	return hmac.new(settings.SECRET_KEY, mash).hexdigest()
 
 def calc_open_mac(timestamp, recipient, email_id):
