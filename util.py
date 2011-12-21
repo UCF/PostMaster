af4f13eaf963967f0045ad74efbd1cd87d03dbae
Christopher Conover <clcono@gmail.com>
2011-12-21 14:09:08 -0500

    Removed timestamp field from url_mac calculation

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 import hmac
 
-def calc_url_mac(url, position, recipient, timestamp):
-	mash = ''.join([str(url), str(position), str(recipient), str(timestamp)])
+def calc_url_mac(url, position, recipient):
+	mash = ''.join([str(url), str(position), str(recipient))
 	return hmac.new(settings.SECRET_KEY, mash).hexdigest()
