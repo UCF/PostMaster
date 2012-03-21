d8771e6634334d3e2c0b2e9928afc035cd69cd51
Chris Conover <clcono@gmail.com>
2012-03-21 13:45:11 -0400

    Removed password change form

--- urls.py
+++ urls.py
@@ -6,8 +6,6 @@ urlpatterns = patterns('',
 	url(r'', include('mailer.urls')),
 	url(r'^login/$', view='django.contrib.auth.views.login', kwargs={'template_name': 'login.html'}, name='login'),
 	url(r'^logout/$', view='django.contrib.auth.views.logout', kwargs={'template_name': 'logout.html'}, name='logout'),
-	url(r'^password_change/$', view='django.contrib.auth.views.password_change', kwargs={'template_name': 'password_change.html'}, name='password-change'),
-	url(r'^password_change_done/$', view='django.contrib.auth.views.password_change_done', kwargs={'template_name': 'password_change_done.html'}, name='password-change-done'),
 )
 
 handler500 = lambda r: direct_to_template(r, template='500.html')
