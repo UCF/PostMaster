247f52c12da68f14c93311daa140f4221bf3c16e
Chris Conover <clcono@gmail.com>
2012-08-06 09:16:43 -0400

    Removed existing forms, urls and views

--- mailer/urls.py
+++ mailer/urls.py
@@ -1,15 +1,4 @@
 from django.conf.urls.defaults   import patterns, include, url
-from django.views.generic.simple import direct_to_template
-from django.conf                 import settings
 
-urlpatterns = patterns('mailer.views',
-	url(r'^email/unsubscribe/?$', view='unsubscribe', name='mailer-email-unsubscribe'),
-	url(r'^email/open/?$', view='instance_open', name='mailer-email-open'),
-	url(r'^email/redirect/?$', view='redirect', name='mailer-email-redirect'),
-	url(r'^email/(?P<email_id>\d+)/deactivate/?$', view='deactivate', name='mailer-email-deactivate'),
-	url(r'^email/(?P<email_id>\d+)/details/?$', view='details', name='mailer-email-details'),
-	url(r'^email/(?P<email_id>\d+)/map/?$', view='map_labels_fields', name='mailer-email-map'),
-	url(r'^email/(?P<email_id>\d+)/update/?$', view='create_update_email', name='mailer-email-update'),
-	url(r'^email/create/?$', view='create_update_email', name='mailer-email-create'),
-	url(r'^$', view='list_emails', name='mailer-email-list'),
+urlpatterns = patterns('',
 )
