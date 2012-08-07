02ea207d57ca7719a818ffa84eb6f929212b7e8f
Chris Conover <clcono@gmail.com>
2012-08-07 11:38:35 -0400

    Remove HTML clean checking

--- manager/forms.py
+++ manager/forms.py
@@ -2,13 +2,7 @@ from django        import forms
 from manager.models import Email
 
 class EmailCreateForm(forms.ModelForm):
-
-	def clean(self):
-		cleaned_data = super(EmailCreateForm, self).clean()
-		if cleaned_data['html'] == '' and cleaned_data['source_uri'] == '':
-			raise forms.ValidationError('Either the HTML and Source URI field must not be blank.')
-		return cleaned_data
-
+	
 	class Meta:
 		model   = Email
 		exclude = ('unsubscriptions', )
