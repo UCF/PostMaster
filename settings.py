beb990b83736da792f4743ed8d7c0cc88a9e6841
Chris Conover <clcono@gmail.com>
2012-08-07 09:55:25 -0400

    Spaces to tabs

--- settings.py
+++ settings.py
@@ -16,24 +16,24 @@ USE_I18N          = False
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
+	'django.template.loaders.filesystem.Loader',
+	'django.template.loaders.app_directories.Loader',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.request",
-    'django.contrib.messages.context_processors.messages',
+	"django.contrib.auth.context_processors.auth",
+	"django.core.context_processors.debug",
+	"django.core.context_processors.i18n",
+	"django.core.context_processors.media",
+	"django.core.context_processors.request",
+	'django.contrib.messages.context_processors.messages',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 # Add local apps folder to python path
@@ -41,82 +41,82 @@ sys.path.append(APP_FOLDER)
 sys.path.append(INC_FOLDER)
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'mailer',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.sites',
+	'mailer',
 )
 
 AUTHENTICATION_BACKENDS = (
-    'mailer.auth.Backend',
+	'mailer.auth.Backend',
 )
 
 LOGGING = {
-    'version':1,
-    'disable_existing_loggers':True,
-    'filters': {
-        'require_debug_true': {
-            '()': 'logs.RequiredDebugTrue',
-        },
-        'require_debug_false': {
-            '()': 'logs.RequiredDebugFalse',
-        }
-    },
-    'formatters': {
-        'talkative': {
-            'format':'[%(asctime)s] %(levelname)s:%(module)s %(funcName)s %(lineno)d %(message)s'
-        },
-        'concise': {
-            'format':'%(levelname)s: %(message)s (%(asctime)s)'
-        }
-    },
-    'handlers': {
-        'discard': {
-            'level':'DEBUG',
-            'class':'django.utils.log.NullHandler'
-        },
-        'console': {
-            'level':'DEBUG',
-            'class':'logging.StreamHandler',
-            'formatter':'talkative',
-            'filters': ['require_debug_true']
-        },
-        'file': {
-            'level': 'INFO',
-            'class':'logging.FileHandler',
-            'filename': os.path.join(PROJECT_FOLDER,'logs', 'application.log'),
-            'formatter':'concise',
-            'filters': ['require_debug_false']
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers':['discard'],
-            'propogate': True,
-            'level':'INFO'
-        },
-        'mailer': {
-            'handlers':['console', 'file'],
-            'propogate': True,
-            'level':'DEBUG'
-        },
-    }
+	'version':1,
+	'disable_existing_loggers':True,
+	'filters': {
+		'require_debug_true': {
+			'()': 'logs.RequiredDebugTrue',
+		},
+		'require_debug_false': {
+			'()': 'logs.RequiredDebugFalse',
+		}
+	},
+	'formatters': {
+		'talkative': {
+			'format':'[%(asctime)s] %(levelname)s:%(module)s %(funcName)s %(lineno)d %(message)s'
+		},
+		'concise': {
+			'format':'%(levelname)s: %(message)s (%(asctime)s)'
+		}
+	},
+	'handlers': {
+		'discard': {
+			'level':'DEBUG',
+			'class':'django.utils.log.NullHandler'
+		},
+		'console': {
+			'level':'DEBUG',
+			'class':'logging.StreamHandler',
+			'formatter':'talkative',
+			'filters': ['require_debug_true']
+		},
+		'file': {
+			'level': 'INFO',
+			'class':'logging.FileHandler',
+			'filename': os.path.join(PROJECT_FOLDER,'logs', 'application.log'),
+			'formatter':'concise',
+			'filters': ['require_debug_false']
+		}
+	},
+	'loggers': {
+		'django': {
+			'handlers':['discard'],
+			'propogate': True,
+			'level':'INFO'
+		},
+		'mailer': {
+			'handlers':['console', 'file'],
+			'propogate': True,
+			'level':'DEBUG'
+		},
+	}
 }
 
 DOT = ''
 with open(os.path.join(PROJECT_FOLDER, 'static', 'img', 'dot.png'), 'r') as dot:
-    DOT = dot.read()
+	DOT = dot.read()
 
 try:
-    from settings_local import *
+	from settings_local import *
 except ImportError:
-    from django.core.exceptions import ImproperlyConfigured
-    raise ImproperlyConfigured(
-        'Local settings file was not found. ' +
-        'Ensure settings_local.py exists in project root.'
-    )
+	from django.core.exceptions import ImproperlyConfigured
+	raise ImproperlyConfigured(
+		'Local settings file was not found. ' +
+		'Ensure settings_local.py exists in project root.'
+	)
 
 
 TEMPLATE_DEBUG = DEBUG
