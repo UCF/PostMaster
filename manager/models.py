a7a8cfc0477381e2972cf00f8454f76bc6a853fd
Chris Conover <clcono@gmail.com>
2012-08-07 11:37:30 -0400

    Required source uri field

--- manager/models.py
+++ manager/models.py
@@ -110,7 +110,6 @@ class Email(models.Model):
 		'active'            : 'Whether the email is active or not. Inactive emails will not be sent',
 		'title'             : 'Internal identifier of the email',
 		'subject'           : 'Subject of the email',
-		'html'              : 'HTML source code of the email content',
 		'source_uri'        : 'Source URI of the email content',
 		'start_date'        : 'Date that the email will first be sent.',
 		'send_time'         : 'Format: %H:%M or %H:%M:%S. Time of day when the email will be sent. Times will be rounded to the nearest quarter hour.',
@@ -128,8 +127,7 @@ class Email(models.Model):
 	active             = models.BooleanField(default=False, help_text=_HELP_TEXT['active'])
 	title              = models.CharField(max_length=100, help_text=_HELP_TEXT['title'])
 	subject            = models.CharField(max_length=998, help_text=_HELP_TEXT['subject'])
-	html               = models.TextField(blank=True, null=True, help_text=_HELP_TEXT['html'])
-	source_uri         = models.URLField(blank=True, null=True, help_text=_HELP_TEXT['source_uri'])
+	source_uri         = models.URLField(help_text=_HELP_TEXT['source_uri'])
 	start_date         = models.DateField(help_text=_HELP_TEXT['start_date'])
 	send_time          = models.TimeField(help_text=_HELP_TEXT['send_time'])
 	recurrence         = models.SmallIntegerField(null=True, blank=True, default=Recurs.never, choices=Recurs.choices, help_text=_HELP_TEXT['recurrence'])
