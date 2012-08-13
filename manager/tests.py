63b17fbdb141034f083e2e99dea2a296933a35b4
Chris Conover <clcono@gmail.com>
2012-08-13 09:21:06 -0400

    Initial test case

--- manager/tests.py
+++ manager/tests.py
@@ -5,12 +5,17 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test    import TestCase
+from manager.models import Recipient
 
+class RecipientTestCase(TestCase):
+	def setUp(self):
+		self.recipient = Recipient.objects.create(email_address='christopher.conover@ucf.edu')
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+	def test_hmac_hash(self):
+		'''
+			Make sure the hmac has is not blank and is 32 chars in length.
+		'''
+		hmac = self.recipient.hmac_hash
+		self.assertTrue(hmac != '')
+		self.assertTrue(len(hmac) == 32)
