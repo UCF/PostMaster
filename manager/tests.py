bcfc01da9e7763349db91ad3920c4b8214885854
Chris Conover <clcono@gmail.com>
2012-08-13 09:34:28 -0400

    more recipient tests

--- manager/tests.py
+++ manager/tests.py
@@ -6,11 +6,16 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test    import TestCase
-from manager.models import Recipient
+from manager.models import Recipient, RecipientAttribute, RecipientGroup
 
 class RecipientTestCase(TestCase):
 	def setUp(self):
 		self.recipient = Recipient.objects.create(email_address='christopher.conover@ucf.edu')
+		self.attribute = RecipientAttribute.objects.create(
+			recipient=self.recipient,
+			name='first_name',
+			value='Chris')
+		self.group     = RecipientGroup.objects.create(name='Test Group')
 
 	def test_hmac_hash(self):
 		'''
@@ -19,3 +24,16 @@ class RecipientTestCase(TestCase):
 		hmac = self.recipient.hmac_hash
 		self.assertTrue(hmac != '')
 		self.assertTrue(len(hmac) == 32)
+
+	def test_subscriptions(self):
+		'''
+			Should be zero here
+		'''
+		self.assertTrue(len(self.recipient.subscriptions) == 0)
+
+	def test_existing_attribute(self):
+		self.assertEqual(self.recipient.first_name, 'Chris')
+
+	def test_missing_attribute(self):
+		with self.assertRaises(AttributeError):
+			self.recipient.blah
