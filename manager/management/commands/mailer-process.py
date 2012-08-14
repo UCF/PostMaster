7127aaf3cef5ed21f3343351d52c6c3de8fb38fd
Chris Conover <clcono@gmail.com>
2012-08-14 15:34:28 -0400

    Updated to account for refactor

--- manager/management/commands/mailer-process.py
+++ manager/management/commands/mailer-process.py
@@ -1,14 +1,20 @@
 from django.core.management.base import BaseCommand
 from manager.models              import Email
-
+from datetime                    import datetime
 log = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
 	'''
-		Handles sending emails. Should be 
-		set to run every 15 minutes
+		Handles sending emails. Should be set to run according to the
+		PROCESSING_INTERVAL_DURATION setting in settings_local.py
 	'''
 
 	def handle(self, *args, **options):
-		pass
+		now = datetime.now()
+
+		for email in Email.objects.previewing_now(now=now):
+			email.preview()
+
+		for email in Email.objects.sending_now(now=now):
+			email.send()
