2a4668f5a0d61fca0ca4410c917ab4feecf6f787
Chris Conover <clcono@gmail.com>
2012-08-16 15:10:13 -0400

    Added logout template

--- urls.py
+++ urls.py
@@ -5,7 +5,7 @@ from django.conf                 import settings
 urlpatterns = patterns('',
 	url(r'', include('manager.urls')),
 	url(r'^login/$', view='django.contrib.auth.views.login', kwargs={'template_name': 'login.html'}, name='login'),
-	url(r'^logout/$', view='django.contrib.auth.views.logout', kwargs={'template_name': 'login.html'}, name='logout'),
+	url(r'^logout/$', view='django.contrib.auth.views.logout', kwargs={'template_name': 'logout.html'}, name='logout'),
 )
 
 handler500 = lambda r: direct_to_template(r, template='500.html')
