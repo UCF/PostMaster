acad50b27a7b11405456877666785ee9a3eefe42
Chris Conover <clcono@gmail.com>
2012-08-16 10:55:48 -0400

    Required login for private manager views

--- manager/urls.py
+++ manager/urls.py
@@ -1,24 +1,25 @@
-from django.conf.urls.defaults   import patterns, include, url
-from manager.views               import *
-from django.views.generic.simple import direct_to_template
+from django.conf.urls.defaults      import patterns, include, url
+from manager.views                  import *
+from django.views.generic.simple    import direct_to_template
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('manager.views',
 
 	# Emails
-	url(r'^email/unsubscribe/?$',           view='unsubscribe',          name='manager-email-unsubscribe'),
+	url(r'^email/unsubscribe/?$',          view='unsubscribe',           name='manager-email-unsubscribe'),
 	url(r'^email/open/?$',                 view='instance_open',         name='manager-email-open'),
 	url(r'^email/redirect/?$',             view='redirect',              name='manager-email-redirect'),
-	url(r'^email/create/$',                EmailCreateView.as_view(),    name='manager-email-create'),
-	url(r'^email/(?P<pk>\d+)/update/$',    EmailUpdateView.as_view(),    name='manager-email-update'),
-	url(r'^email/(?P<pk>\d+)/instances/$', InstanceListView.as_view(),   name='manager-email-instances'),
-	url(r'^email/instance/(?P<pk>\d+)/$',  InstanceDetailView.as_view(), name='manager-email-instance'),
-	url(r'^emails/$',                      EmailListView.as_view(),      name='manager-emails'),
+	url(r'^email/create/$',                login_required(EmailCreateView.as_view()),    name='manager-email-create'),
+	url(r'^email/(?P<pk>\d+)/update/$',    login_required(EmailUpdateView.as_view()),    name='manager-email-update'),
+	url(r'^email/(?P<pk>\d+)/instances/$', login_required(InstanceListView.as_view()),   name='manager-email-instances'),
+	url(r'^email/instance/(?P<pk>\d+)/$',  login_required(InstanceDetailView.as_view()), name='manager-email-instance'),
+	url(r'^emails/$',                      login_required(EmailListView.as_view()),      name='manager-emails'),
 
 	# Recipients
-	url(r'^recipientgroups/$',                       RecipientGroupListView.as_view(),   name='manager-recipientgroups'),
-	url(r'^recipientgroup/create/$',                 RecipientGroupCreateView.as_view(), name='manager-recipientgroup-create'),
-	url(r'^recipientgroup/(?P<pk>\d+)/recipients/$', RecipientListView.as_view(),        name='manager-recipientgroup-recipients'),
-	url(r'^recipient/(?P<pk>\d+)/$',                 RecipientDetailView.as_view(),      name='manager-recipient'),
+	url(r'^recipientgroups/$',                       login_required(RecipientGroupListView.as_view()),   name='manager-recipientgroups'),
+	url(r'^recipientgroup/create/$',                 login_required(RecipientGroupCreateView.as_view()), name='manager-recipientgroup-create'),
+	url(r'^recipientgroup/(?P<pk>\d+)/recipients/$', login_required(RecipientListView.as_view()),        name='manager-recipientgroup-recipients'),
+	url(r'^recipient/(?P<pk>\d+)/$',                 login_required(RecipientDetailView.as_view()),      name='manager-recipient'),
 
-	url(r'^$', direct_to_template, kwargs={'template':'manager/instructions.html'}, name='manager-home'),
+	url(r'^$', login_required(direct_to_template), kwargs={'template':'manager/instructions.html'}, name='manager-home'),
 )
