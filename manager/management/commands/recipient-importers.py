b7d0056379101d48e0aa7703ed8931f12b888a28
Chris Conover <clcono@gmail.com>
2012-08-20 14:57:57 -0400

    Fixed indentation error

--- manager/management/commands/recipient-importers.py
+++ manager/management/commands/recipient-importers.py
@@ -1,4 +1,4 @@
-	from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand, CommandError
 from optparse                    import make_option
 from manager.models              import Recipient, RecipientGroup
 from django.conf                 import settings
