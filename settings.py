b38e846493c121030f8025c3de180eb30653a351
Chris Conover <clcono@gmail.com>
2012-08-20 09:21:36 -0400

    Removed admin installed app

--- settings.py
+++ settings.py
@@ -43,7 +43,6 @@ sys.path.append(APP_FOLDER)
 sys.path.append(INC_FOLDER)
 
 INSTALLED_APPS = (
-	'django.contrib.admin',
 	'django.contrib.auth',
 	'django.contrib.contenttypes',
 	'django.contrib.sessions',
