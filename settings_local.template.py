645136eea4e894a22c3cff58b5fae4c5f2672b6d
Chris Conover <clcono@gmail.com>
2012-08-22 14:46:00 -0400

    Removed LDAP_NET_SEARCH_USER and LDAP_NET_SEARCH_PASS settings

--- settings_local.template.py
+++ settings_local.template.py
@@ -70,8 +70,6 @@ LDAP_NET_ATTR_MAP    = { # LDAP Object -> User Object
 	'sn'        : 'sn',
 	'mail'      : 'email'
 }
-LDAP_NET_SEARCH_USER = ''
-LDAP_NET_SEARCH_PASS = ''
 
 # NID of users who are allowed to manage Postmaster
 MANAGERS = ()
