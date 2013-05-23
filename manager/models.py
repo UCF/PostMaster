e1b4d65db6f79f3c77f6cf713b7226eda3fec3d1
Brandon Groves <brandon.groves@ucf.edu>
2013-05-23 15:44:10 -0400

    re-worded some of the preview descriptions

--- manager/models.py
+++ manager/models.py
@@ -295,7 +295,7 @@ class Email(models.Model):
         'from_friendly_name': 'A display name associated with the from email address',
         'track_urls': 'Rewrites all URLs in the email content to be recorded',
         'track_opens': 'Adds a tracking image to email content to track if and when an email is opened.',
-        'preview': 'BE CAREFUL! This could cause a live email to be sent on the next send cycle.\nSend a preview to a specific set of individuals allowing them to proof the content.',
+        'preview': 'BE CAREFUL! Un-checking this value will disable sending the preview email. In some cases, if un-checked, this will cause a live email to be sent on the next send cycle.\nSend a preview to a specific set of individuals allowing them to proof the content.',
         'preview_recipients': 'A comma-separated list of preview recipient email addresses'
     }
 
@@ -425,11 +425,10 @@ class Email(models.Model):
             <div style="background-color:#000;color:#FFF;font-size:18px;padding:20px;">
                 This is a preview of an email that will go out at approximately ''' + self.live_est_time.strftime('%I:%M %p') + '''
                 <br /><br />
-                The content of the email when it is sent will be re-requested from
-                the source for the real delivery.
+                The content of this email may be change before the final email is sent. Any changes that are made to the content below will be included on the live email.
             </div>
         '''
-        text_explanation = 'This is a preview of an email that will go out at approximately ' + self.live_est_time.strftime('%I:%M %p') + '.\n\nThe content of the email when it is sent will be re-requested from the source for the real delivery.'
+        text_explanation = 'This is a preview of an email that will go out at approximately ' + self.live_est_time.strftime('%I:%M %p') + '.\n\nThe content of this email may be change before the final email is sent. Any changes that are made to the content below will be included on the live email'
 
         try:
             amazon = smtplib.SMTP_SSL(settings.AMAZON_SMTP['host'], settings.AMAZON_SMTP['port'])
