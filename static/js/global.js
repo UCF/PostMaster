9c7ff66700cc64131657f1c174ecbfccfa144de0
RJ Bruneel <rj.bruneel@ucf.edu>
2015-03-19 13:28:18 -0400

    added pagination to bottom of pages

--- static/js/global.js
+++ static/js/global.js
@@ -6,7 +6,10 @@
   }
 
   function init() {
+    // Table row click handler
     $('table').on('click', 'td:not(:has(a))', tableClickHandler);
+    // Select the first form input
+    $('form:first *:input[type!=hidden]:input[type!=checkbox]:first').focus();
   }
 
   $(init);
