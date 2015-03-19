95c1f46e0a521904c0786ef4cb3dc4efabe6f506
RJ Bruneel <rj.bruneel@ucf.edu>
2015-03-19 09:06:42 -0400

    ui updates

--- static/js/global.js
+++ static/js/global.js
@@ -1,20 +1,14 @@
-$().ready(function() {
+(function() {
+  'use strict';
 
-	/**
-	 * If a <table> <tr> has a data-link-url attribute, make a click of that row
-	 * link to the data-link-url value. Also, while hoving over that row, add the
-	 * active class.
-	 **/
-	$('tr')
-		.each(function(index, row) {
-			var row      = $(row),
-				link_url = row.attr('data-link-url');
-			if(typeof link_url != 'undefined') {
-				row.hover(
-					function() {$(this).addClass('active');},
-					function() {$(this).removeClass('active');}
-				);
-				row.find('td:not(:has(a))').click(function() {window.location = link_url});
-			}
-		})
-});
+  function tableClickHandler(e) {
+    window.location = $(e.target).parent().attr('data-link-url');
+  }
+
+  function init() {
+    $('table').on('click', 'td:not(:has(a))', tableClickHandler);
+  }
+
+  $(init);
+
+}());
