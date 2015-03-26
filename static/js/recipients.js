29ce31038a06f272491e8e6342a430d82e5fc92e
RJ Bruneel <rj.bruneel@ucf.edu>
2015-03-26 10:27:22 -0400

    updated pagination and recipient attributes

--- static/js/recipients.js
+++ static/js/recipients.js
@@ -3,13 +3,19 @@
 
   var recipientCreatePage = false,
       $attributeContainer = $('.attribute-container'),
+      $addAttrContainer = $('.add-attr-container'),
       $clonedAttribute;
 
   function reorderAttributes() {
     $attributeContainer.find('.attribute').each(function( index ) {
       $(this).find('input').eq(0).attr('id','id_attributes-' + index + '-name').attr('name','id_attributes-' + index + '-value');
       $(this).find('input').eq(1).attr('id','id_attributes-' + index + '-name').attr('name','id_attributes-' + index + '-value');
+      $(this).find('input').eq(2).attr('id','id_attributes-' + index + '-name').attr('name','id_attributes-' + index + '-value');
     });
+  }
+
+  function updateTotalForms() {
+    $('#id_attributes-TOTAL_FORMS').val($attributeContainer.find('.attribute').length);
   }
 
   function incrementNumberInString(string) {
@@ -31,26 +37,22 @@
     if(recipientCreatePage) {
       reorderAttributes();
     }
+    updateTotalForms();
   }
 
   function deleteAttribute(e) {
     e.preventDefault();
     if(recipientCreatePage) {
-      $(e.target).closest('.attribute').remove();
       reorderAttributes();
     } else {
-      $(e.target)
-        .next().prop( "checked", true )
-        .closest('.attribute').fadeOut();
+      $(e.target).next().prop( "checked", true );
     }
+    $(e.target).closest('.attribute').fadeOut();
+    updateTotalForms();
   }
 
-  function highlightRowMouseOver(e) {
-    $(e.target).closest('.form-group').parent().find('.row').addClass('alert-' + $(e.target).attr('data-color'));
-  }
-
-  function highlightRowMouseOut(e) {
-    $(e.target).closest('.form-group').parent().find('.row').removeClass('alert-' + $(e.target).attr('data-color'));
+  function highlightRowToggle(e) {
+    $(e.target).closest('.form-group').toggleClass('alert-' + $(e.target).attr('data-color'));
   }
 
   function init() {
@@ -61,11 +63,13 @@
     // Add attribute button
     $('.add-attr-btn').on('click', addAttribute);
     // Delete attribute button
-    $attributeContainer.on('click', '.delete-attr-btn', deleteAttribute);
+    $attributeContainer.on('click', 'span', deleteAttribute);
     // mouse over event
-    $attributeContainer.on('mouseover', '.delete-attr-btn, .add-attr-btn', highlightRowMouseOver);
+    $attributeContainer.on('mouseover', 'span', highlightRowToggle);
+    $addAttrContainer.on('mouseover', 'span', highlightRowToggle);
     // mouse out event
-    $attributeContainer.on('mouseout', '.delete-attr-btn, .add-attr-btn', highlightRowMouseOut);
+    $attributeContainer.on('mouseout', 'span', highlightRowToggle);
+    $addAttrContainer.on('mouseout', 'span', highlightRowToggle);
   }
 
   $(init);
