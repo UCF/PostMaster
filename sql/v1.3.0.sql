fe3f86aff4845c890ee8507fb0ca925e409f118d
Brandon Groves <brandon.groves@gmail.com>
2015-03-26 13:20:09 -0400

    updated the default preview instance sql to using the right format for default

--- sql/v1.3.0.sql
+++ sql/v1.3.0.sql
@@ -3,4 +3,4 @@ alter table manager_instance add litmus_id varchar(100);
 
 ### ADD locked_content and sent_html
 alter table manager_previewinstance add sent_html text;
-alter table manager_previewinstance add lock_content tinyint(1) not null default(0);
+alter table manager_previewinstance add lock_content tinyint(1) not null default 0;
