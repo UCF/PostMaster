861d632e000a82fc306013a405e0166e74e1c294
Jim Barnes <james.barnes@ucf.edu>
2015-03-30 08:16:49 -0400

    Moved javascript on recipientgroup-update page to it's own file.

---
+++ static/js/recipientgroup-update.js
@@ -0,0 +1,27 @@
+(function() {
+    $('#tb_recipient_search').focus();
+
+    var recipients = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('email_address'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            remote: '/recipients/json/?search=%QUERY'
+        });
+
+        recipients.clearPrefetchCache();
+
+        recipients.initialize();
+
+        $('#tb_recipient_search').typeahead({
+            hint: false,
+            minLength: 3,
+            limit: 10,
+            highlight: true
+        },{
+            displayKey: 'email_address',
+            source: recipients.ttAdapter()
+        });
+
+        $('#tb_recipient_search').bind('typeahead:selected', function(obj, datum, name) {
+            $('#tb_recipient_search').val(datum.email_address);
+        });
+})();
