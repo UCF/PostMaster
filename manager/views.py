8f76299d559c8884b54555454e0450c08d9c5572
Jim Barnes <james.barnes@ucf.edu>
2015-05-20 10:38:53 -0400

    Added ability to create recipients from recipient group update screen.

--- manager/views.py
+++ manager/views.py
@@ -415,7 +415,7 @@ class RecipientGroupUpdateView(RecipientGroupsMixin, UpdateView):
 
         if recipient_email:
             recipient_group = RecipientGroup.objects.get(pk=self.object.pk)
-            recipient = Recipient.objects.get(email_address=recipient_email)
+            recipient = Recipient.objects.filter(email_address=recipient_email)
             if recipient:
                 if recipient not in recipient_group.recipients.all():
                     recipient_group.recipients.add(recipient)
@@ -423,8 +423,12 @@ class RecipientGroupUpdateView(RecipientGroupsMixin, UpdateView):
                 else:
                     messages.warning(self.request, 'Recipient %s already in %s.' % (recipient_email, recipient_group.name))
             else:
-                messages.error(self.request, 'Recipient with email address %s does not exist.' % recipient_email)
-                return super(RecipientGroupUpdateView, self).form_invalid(form)
+                recipient = Recipient(email_address=recipient_email)
+                recipient.save()
+                recipient_group.recipients.add(recipient)
+                recipient_group.save()
+                messages.success(self.request, 'Recipient %s successfully created and added to %s.' % (recipient_email, recipient_group.name))
+                return super(RecipientGroupUpdateView, self).form_valid(form)
 
         messages.success(self.request, 'Recipient group successfully updated.')
         return super(RecipientGroupUpdateView, self).form_valid(form)
