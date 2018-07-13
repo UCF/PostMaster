e8102fd8af86aa8dd38528020ff3b7c04293154f
RJ Bruneel <rj.bruneel@ucf.edu>
2018-07-13 10:42:33 -0400

    Updated and fixed typeahead logic

--- static/js/recipientgroup-update.js
+++ static/js/recipientgroup-update.js
@@ -1,29 +1,34 @@
-(function() {
-    $('#tb_recipient_search').focus();
+(function () {
+  $('#tb_recipient_search').focus();
 
-    // TODO: fix type ahead
+  function getQuery() {
+    return $('#tb_recipient_search').val();
+  }
 
-    // var recipients = new Bloodhound({
-    //         datumTokenizer: Bloodhound.tokenizers.obj.whitespace('email_address'),
-    //         queryTokenizer: Bloodhound.tokenizers.whitespace,
-    //         remote: '/recipients/json/?search=%QUERY'
-    //     });
+  var recipients = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('email_address'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+      wildcard: '%QUERY',
+      url: '/recipients/json/?search=%QUERY'
+    }
+  });
 
-    //     recipients.clearPrefetchCache();
+  recipients.clearPrefetchCache();
 
-    //     recipients.initialize();
+  recipients.initialize();
 
-    //     $('#tb_recipient_search').typeahead({
-    //         hint: false,
-    //         minLength: 3,
-    //         limit: 10,
-    //         highlight: true
-    //     },{
-    //         displayKey: 'email_address',
-    //         source: recipients.ttAdapter()
-    //     });
+  $('#tb_recipient_search').typeahead({
+    hint: false,
+    minLength: 3,
+    limit: 10,
+    highlight: true
+  }, {
+      displayKey: 'email_address',
+      source: recipients.ttAdapter()
+    });
 
-    //     $('#tb_recipient_search').bind('typeahead:selected', function(obj, datum, name) {
-    //         $('#tb_recipient_search').val(datum.email_address);
-    //     });
+  $('#tb_recipient_search').on('typeahead:selected', function (obj, datum, name) {
+    $('#tb_recipient_search').val(datum.email_address);
+  });
 })();
