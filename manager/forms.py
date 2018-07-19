8d43c1e03df9cb84940268ed54c53b736e556081
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-07-19 10:03:04 -0400

    Added base forms for managing subscription categories

--- manager/forms.py
+++ manager/forms.py
@@ -7,6 +7,7 @@ from manager.models import Recipient
 from manager.models import RecipientAttribute
 from manager.models import RecipientGroup
 from manager.models import Setting
+from manager.models import SubscriptionCategory
 
 
 class EmailSearchForm(forms.Form):
@@ -148,6 +149,15 @@ class RecipientSubscriptionsForm(forms.ModelForm):
         model = Recipient
         fields = ()
 
+class SubscriptionCategoryForm(forms.ModelForm):
+    class Meta:
+        model = SubscriptionCategory
+        fields = (
+            'name',
+            'description',
+            'cannot_unsubscribe',
+            'applies_to'
+        )
 
 class SettingCreateUpdateForm(forms.ModelForm):
 
--- manager/views.py
+++ manager/views.py
@@ -48,6 +48,7 @@ from manager.forms import RecipientGroupUpdateForm
 from manager.forms import RecipientSearchForm
 from manager.forms import RecipientSubscriptionsForm
 from manager.forms import SettingCreateUpdateForm
+from manager.forms import SubscriptionCategoryForm
 from manager.models import Email
 from manager.models import Instance
 from manager.models import InstanceOpen
@@ -56,6 +57,7 @@ from manager.models import RecipientAttribute
 from manager.models import Recipient
 from manager.models import RecipientGroup
 from manager.models import Setting
+from manager.models import SubscriptionCategory
 from manager.models import URL
 from manager.models import URLClick
 from manager.litmusapi import LitmusApi
@@ -140,6 +142,13 @@ class SettingsMixin(object):
     def get_context_data(self, **kwargs):
         context = super(SettingsMixin, self).get_context_data(**kwargs)
         context['section'] = 'settings'
+        return context
+
+
+class SubscriptionsMixin(object):
+    def get_context_data(self, **kwargs):
+        context = super(SubscriptionsMixin, self).get_context_data(**kwargs)
+        context['section'] = 'subscriptions'
         return context
 
 
@@ -872,6 +881,38 @@ class RecipientSubscriptionsUpdateView(UpdateView):
                          'Your subscriptions have been successfully updated.')
         return self.object.unsubscribe_url
 
+class SubscriptionCategoryListView(SubscriptionsMixin, ListView):
+    model = SubscriptionCategory
+    template_name = 'manager/subscription-category-list.html'
+    context_object_name = 'categories'
+
+class SubscriptionCategoryCreateView(SubscriptionsMixin, CreateView):
+    model = SubscriptionCategory
+    template_name = 'manager/subscription-category-create.html'
+    form_class = SubscriptionCategoryForm
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Subscription Category successfully created.')
+        return super(SubscriptionCategoryCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('manager-subscription-categories-update',
+                      args=(),
+                      kwargs={'pk': self.object.pk})
+
+class SubscriptionCategoryUpdateView(SubscriptionsMixin, UpdateView):
+    model= SubscriptionCategory
+    template_name = 'manager/subscription-category-update.html'
+    form_class = SubscriptionCategoryForm
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Subscription Category successfully updated.')
+        return super(SubscriptionCategoryUpdateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('manager-subscription-categories-update',
+                      args=(),
+                      kwargs={'pk': self.object.pk})
 
 class SettingListView(SettingsMixin, ListView):
     model = Setting
@@ -893,7 +934,6 @@ class SettingCreateView(SettingsMixin, CreateView):
         return reverse('manager-setting-update',
                        args=(),
                        kwargs={'pk': self.object.pk})
-
 
 class SettingUpdateView(SettingsMixin, UpdateView):
     model = Setting
