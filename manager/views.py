7ea1b154e6d875757448c82eccd73f608980a1d7
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-07-23 15:07:42 -0400

    Added disabled checkbox to make it more clear the subscription category cannot be unsubscribed from.

--- manager/views.py
+++ manager/views.py
@@ -869,8 +869,6 @@ class RecipientSubscriptionsUpdateView(UpdateView):
         subscriptions = form.cleaned_data['subscription_categories']
         unsubscriptions = list(set(SubscriptionCategory.objects.all()) - set(subscriptions))
 
-        print unsubscriptions
-
         for category in unsubscriptions:
             category.unsubscriptions.add(self.object)
 
