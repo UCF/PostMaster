b397c4a9619c4e01ab69b1d399760f7c81d25993
Jim Barnes <jimbarnesdeveloper@gmail.com>
2018-07-24 12:03:55 -0400

    Added return if the dom element doesn't exist on the page

--- src/js/script/subscription-update.js
+++ src/js/script/subscription-update.js
@@ -1,4 +1,6 @@
 var subscriptionUpdate = function($) {
+    if ($('#unsubscribe-all').length === 0) return;
+
     $('#unsubscribe-all').on('click', function() {
         $('input[type="checkbox"][name="subscription_categories"]').prop('checked', false);
     });
