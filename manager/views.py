769d0d9b10a13d47e16c2fed9b8eee50b575a8d5
Jo Dickson <jogreybill@gmail.com>
2020-10-05 14:12:22 -0400

    Fixed usage of .count() instead of len() when retrieving email count in email placeholder verification view

--- manager/views.py
+++ manager/views.py
@@ -329,8 +329,8 @@ class EmailPlaceholderVerificationView(EmailsMixin, DetailView):
         context['attributes'] = []
         for placeholder in placeholders:
             emails = self.object.recipients.exclude(attributes__name=placeholder)
-            if emails.count > 0:
-                context['attributes'].append((placeholder, emails[:10], emails.count))
+            if len(emails) > 0:
+                context['attributes'].append((placeholder, emails[:10], len(emails)))
         return context
 
 
