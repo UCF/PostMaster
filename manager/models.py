e6f963bf987529d04aa4b90e56f642cfbbeec8f0
Jo Dickson <jogreybill@gmail.com>
2022-01-11 15:52:07 -0500

    Updated logic that determines whether or not an email is sending today to ensure emails scheduled in advance don't kick off earlier than intended

--- manager/models.py
+++ manager/models.py
@@ -450,9 +450,9 @@ class EmailManager(models.Manager):
                 # Daily
                 Q(Q(recurrence=self.model.Recurs.daily) & Q(start_date__lte=today)) |
                 # Weekly
-                Q(Q(recurrence=self.model.Recurs.weekly) & Q(start_date__week_day=today.isoweekday() % 7 + 1)) |
+                Q(Q(recurrence=self.model.Recurs.weekly) & Q(start_date__week_day=today.isoweekday() % 7 + 1) & Q(start_date__lte=today)) |
                 # Monthly
-                Q(Q(recurrence=self.model.Recurs.monthly) & Q(start_date__day=today.day))
+                Q(Q(recurrence=self.model.Recurs.monthly) & Q(start_date__day=today.day) & Q(start_date__lte=today))
             ),
             active=True
         )
@@ -489,9 +489,9 @@ class EmailManager(models.Manager):
                 # Daily
                 ~Q(Q(recurrence=self.model.Recurs.daily) & Q(start_date__lte=today)) &
                 # Weekly
-                ~Q(Q(recurrence=self.model.Recurs.weekly) & Q(start_date__week_day=today.isoweekday() % 7 + 1)) &
+                ~Q(Q(recurrence=self.model.Recurs.weekly) & Q(start_date__week_day=today.isoweekday() % 7 + 1) & Q(start_date__lte=today)) &
                 # Monthly
-                ~Q(Q(recurrence=self.model.Recurs.monthly) & Q(start_date__day=today.day))
+                ~Q(Q(recurrence=self.model.Recurs.monthly) & Q(start_date__day=today.day) & Q(start_date__lte=today))
             )
         )
 
