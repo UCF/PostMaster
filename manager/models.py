47c5ad556f0e1957a9d0278225d301647bf83144
Jo Dickson <jogreybill@gmail.com>
2022-02-10 10:54:12 -0500

    Got emoji in subject lines working; added comments

--- manager/models.py
+++ manager/models.py
@@ -1486,10 +1486,17 @@ class EmailMessage:
             self.attach_text(text)
 
     def get_subject(self):
-        # TODO emoji characters are still escaped here :(
+        '''
+        Returns an encoded email header string for the
+        message's subject line.
+        More info: https://docs.aws.amazon.com/ses/latest/dg/send-email-raw.html#send-email-mime-encoding-headers
+        '''
         return '=?utf-8?B?{0}?='.format(self.base64_encode(self.subject))
 
     def get_from(self):
+        '''
+        Returns a formatted "from" name/address for the message.
+        '''
         if self.from_friendly_name:
             return '"{0}" <{1}>'.format(self.from_friendly_name, self.from_address)
         else:
@@ -1497,25 +1504,31 @@ class EmailMessage:
 
     def base64_encode(self, msg_str):
         '''
-        Returns an arbitrary string as base64.
+        Returns a base64-encoded string.
         More info:
         http://docs.aws.amazon.com/ses/latest/DeveloperGuide/send-email-raw.html#send-email-mime-encoding
         '''
         # Normalize back to a string
         msg_str = str(msg_str)
 
-        b64_str = msg_str.encode('unicode-escape')
+        b64_str = msg_str.encode('utf-8')
         b64_str = base64.b64encode(b64_str)
-        b64_str = b64_str.decode('ascii')
+        b64_str = b64_str.decode('utf-8')
 
         return b64_str
 
     def attach_html(self, html):
+        '''
+        Encodes and attaches an HTML string to the message.
+        '''
         self.html = html
         html_encoded = html.encode('utf-8')
         self.msg.attach(MIMEText(html_encoded, 'html', _charset='utf-8'))
 
     def attach_text(self, text):
+        '''
+        Encodes and attaches a plain text string to the message.
+        '''
         self.text = text
         text_encoded = text.encode('utf-8')
         self.msg.attach(MIMEText(text_encoded, 'plain', _charset='utf-8'))
